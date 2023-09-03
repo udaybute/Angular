f16464a299b0b048d3ac8627fb7dce10fba96ebf
Uday Bute <82996279+udaybute@users.noreply.github.com>
2023-09-03 22:11:13 +0530

    Auto-generated commit [ci skip]

--- src/app/directives/open-product-details.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OpenProductDetailsDirective } from './open-product-details.directive';
-
-describe('OpenProductDetailsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new OpenProductDetailsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/directives/open-products.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OpenProductsDirective } from './open-products.directive';
-
-describe('OpenProductsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new OpenProductsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
