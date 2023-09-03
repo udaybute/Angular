f5788a466a474d38cd4b15eceef87cb9b17ef291
Uday Bute <82996279+udaybute@users.noreply.github.com>
2023-09-03 21:31:36 +0530

    Uploaded
    commit changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { OpenProductDirectiveComponent } from './open-product-directive/open-product-directive.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { OpenProductDirectiveComponent } from './open-product-directive/open-pro
     HeaderComponent,
     FooterComponent,
     PageNotFoundComponent,
-    OpenProductDirectiveComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Type, ViewChild, ViewContainerRef } from '@angular/core';
 import { NavigationItem } from '../models/models';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +9,10 @@ import { NavigationItem } from '../models/models';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit{
+
+  @ViewChild('modalTitle') modalTitle !: ElementRef;
+  @ViewChild('container', {read: ViewContainerRef, static:true})
+  container !: ViewContainerRef;
  
   navigationList : NavigationItem[] = [
     {
@@ -24,5 +30,23 @@ export class HeaderComponent implements OnInit{
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
+
+
+  openModal(name: string)
+  {
+    this.container.clear();
+
+    let componentType !: Type<any>;
+    if(name === 'login'){
+      componentType = LoginComponent;
+      this.modalTitle.nativeElement.textContent = "Enter Login Information";
+    }
+    if(name === 'register'){
+       componentType = RegisterComponent;
+       this.modalTitle.nativeElement.textContent = "Enter Register Information";
+    }
+    this.container.createComponent(componentType);
+  }
+   
 
 }
